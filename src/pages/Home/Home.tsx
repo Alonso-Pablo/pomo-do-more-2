c2a04cb756b9665e8221b3c9476b80dfbb46e917
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-06-11 18:40:18 -0300

    another commit

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,6 +9,7 @@ import Panel from '@components/ui/Panel'
 
 import { useClock } from '@hooks/useClock'
 import formatToTimeClock from '@utils/formatToTimeClock'
+import { useState } from 'react'
 
 export default function Home() {
   const {
@@ -19,6 +20,8 @@ export default function Home() {
     restartHandler
   } = useClock(5)
 
+  const [ isDisplayConfig, setIsDisplayConfig ] = useState<boolean>(false)
+
   return (
     <main className="flex h-full py-10 font-main bg-tomato-normal grow">
       <div className="flex flex-col w-full h-full max-w-lg p-5 mx-auto gap-y-10">
@@ -27,30 +30,38 @@ export default function Home() {
           <TaskPanel renderList={TaskList} renderItem={TaskItem} />
         </div>
 
-        {/* Clock */}
         <div className="flex flex-col justify-between gap-y-14 grow">
+          {/* Clock */}
           <Panel
             variant="sunken"
             backClassName="bg-tomato-darkest"
-            frontClassName="flex flex-col bg-tomato-dark text-center text-white p-0 pb-2 gap-y-4"
+            frontClassName="relative flex bg-tomato-dark justify-center p-0 pb-2 cursor-pointer last:justify-self-end"
           >
-              <p className="font-bold text-white select-none text-8xl">{formatToTimeClock(timeRest)}</p>
-              {
-                <div className="flex justify-center px-3 gap-x-5">
-                  <Button onClick={restartHandler} backClassName="bg-tomato-darkest" frontClassName="flex justify-center bg-tomato-normal max-h-[40px]">
-                    <Icon icon="reload" />
-                  </Button>
-                  <Button backClassName="bg-tomato-darkest" frontClassName="flex justify-center bg-tomato-normal max-h-[40px]">
-                    <Icon icon="speaker" />
-                  </Button>
-                  <Button backClassName="bg-tomato-darkest" frontClassName="flex justify-center bg-tomato-normal max-h-[40px]">
-                    <Icon icon="gear" />
-                  </Button>
-                  <Button backClassName="bg-tomato-darkest" frontClassName="flex justify-center bg-tomato-normal max-h-[40px]">
-                    <Icon icon="user" />
-                  </Button>
-                </div>
-              }
+              <div className="flex flex-col gap-y-4">
+                <p className="font-bold text-white select-none text-8xl text-center">{formatToTimeClock(timeRest)}</p>
+                {!isDisplayConfig &&
+                  <div className="flex justify-center px-3 gap-x-5">
+                    <Button onClick={restartHandler} backClassName="bg-tomato-darkest" frontClassName="flex justify-center bg-tomato-normal max-h-[40px]">
+                      <Icon icon="reload" />
+                    </Button>
+                    <Button backClassName="bg-tomato-darkest" frontClassName="flex justify-center bg-tomato-normal max-h-[40px]">
+                      <Icon icon="speaker" />
+                    </Button>
+                    <Button backClassName="bg-tomato-darkest" frontClassName="flex justify-center bg-tomato-normal max-h-[40px]">
+                      <Icon icon="gear" />
+                    </Button>
+                    <Button backClassName="bg-tomato-darkest" frontClassName="flex justify-center bg-tomato-normal max-h-[40px]">
+                      <Icon icon="user" />
+                    </Button>
+                  </div>
+                }
+              </div>
+              <Button variant="text" className="absolute right-0 top-0 bottom-0 bg-tomato-dark" onClick={() => setIsDisplayConfig(!isDisplayConfig)}>
+                <Icon
+                  icon="option-vertical"
+                  className="fill-tomato-darkest px-2"
+                />
+              </Button>
           </Panel>
 
           {/* Two buttons */}
